92920f1f89a18ff0604fc0e8c473560fd10ca147
hightecular95908 <rkulkin47@gmail.com>
2024-08-29 14:51:27 +0900

    added tg_user info in main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,26 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useTelegram } from './hooks/useTelegram';
 
 function App() {
+  const { user } = useTelegram();
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {
+          user ? (
+            <section>
+              <h4>User Name:<span>{user?.username || 'N/A'}</span></h4>
+              <h4>First Name:<span>{user?.first_name || 'N/A'}</span></h4>
+              <h4>Last Name:<span>{user?.last_name || 'N/A'}</span></h4>
+              <h4>Chat Id:<span>{user?.id || 'N/A'}</span></h4>
+            </section>
+          ) : (
+            <p>This app is only available via a telegram.</p>
+          )
+        }
       </header>
     </div>
   );
